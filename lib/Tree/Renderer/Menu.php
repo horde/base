16068e3829423f867b10d49927863e418d95e978
Jan Schneider <jan@horde.org>
2012-06-04 15:11:38 +0200

    Adapt to new Horde_Tree layout. Separate views/renders from logic.

---
+++ lib/Tree/Renderer/Menu.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The Horde_Tree_Menu class renders the tree structure of the top
+ * application menu.
+ *
+ * Copyright 2012 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package  Core
+ */
+class Horde_Tree_Renderer_Menu extends Horde_Tree_Renderer_Base
+{
+    /**
+     * Returns the tree.
+     *
+     * @param boolean $static  Unused.
+     *
+     * @return string  The HTML code of the rendered tree.
+     */
+    public function getTree($static = false)
+    {
+        $view = $GLOBALS['injector']->getInstance('Horde_View');
+        $view->setTemplatePath($GLOBALS['registry']->get('templates', 'horde') . '/tree');
+        $view->rootItems = $this->_tree->getRootNodes();
+        $view->items = $this->_tree->getNodes();
+        return $view->render('menu');
+    }
+}
