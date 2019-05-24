4feac2b196e40f1979d2dfcc858b8c6478dfa388
Jan Schneider <jan@horde.org>
2019-05-24 14:52:06 +0200

    [jan] Fix XSS vulnerabilities in administration interface.

--- templates/admin/config/diff.html.php
+++ templates/admin/config/diff.html.php
@@ -2,8 +2,8 @@
 <p class="notice"><?php echo _("No available configuration data to show differences for.") ?></p>
 <?php else: ?>
 <?php foreach ($this->diffs as $d): ?>
-<h1 class="header" id="<?php echo $d['app'] ?>">
- <?php echo $d['app'] ?>
+<h1 class="header" id="<?php echo htmlspecialchars($d['app']) ?>">
+ <?php echo htmlspecialchars($d['app']) ?>
  <small>[ <?php echo $d['toggle_renderer'] ?> ]</small>
 </h1>
 <pre class="text"><?php echo $d['diff'] ?></pre>
