81a8a11a23a15b3383a8049e1f428a5dbf52f904
boekhorstb1 <91957243+boekhorstb1@users.noreply.github.com>
2022-12-01 11:45:27 +0100

    updating to fix autoloading for some cases

--- rampage.php
+++ rampage.php
@@ -1,4 +1,8 @@
 <?php
-require_once dirname(__FILE__, 3) . '/vendor/autoload.php';
+if (is_dir(dirname(__FILE__, 3) . '/vendor')) {
+    require_once dirname(__FILE__, 3) . '/vendor/autoload.php';
+} elseif (is_dir(dirname(__FILE__, 4) . '/vendor')) {
+    require_once dirname(__FILE__, 4) . '/vendor/autoload.php';
+}
 require_once dirname(__FILE__) . '/lib/core.php';
 Horde\Core\RampageBootstrap::run();
