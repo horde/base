32ebc9e26f9e1e267d1efd45dee482a051fc432b
Torben Dannhauer <torben@dannhauer.de>
2025-05-19 23:10:45 +0200

    Update header.html.php

--- templates/common/header.html.php
+++ templates/common/header.html.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html<?php if (!$this->smartmobileView): ?> PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd"<?php endif; ?>>
 <!DOCTYPE html<?php if (!$this->smartmobileView): ?> PUBLIC "-//W3C//DTD HTML 5.0//EN" "http://www.w3.org/TR/html5/strict.dtd"<?php endif; ?>>
 <html<?php echo $this->htmlAttr ?>>
  <head>
