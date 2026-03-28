541c93de01c58d411a7d51122d91983d14f05a95
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-28 16:00:03 +0100

    chore(release): bump version to 6.0.0-beta8
    Release version 6.0.0-beta8
    
    fix: Need to accept both the modern and the old variant for BC

--- lib/Application.php
+++ lib/Application.php
@@ -31,7 +31,7 @@ if (!class_exists('Horde_Application')) {
     {
         /**
          */
-        public $version = '6.0.0-beta7';
+        public $version = '6.0.0-beta8';
 
         /**
          */
