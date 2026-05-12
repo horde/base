08767046aacfb18ec8b4eb77d38bb57427cf6f77
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-12 20:28:24 +0200

    chore(release): bump version to 6.0.0-RC1
    Release version 6.0.0-RC1
    
    style: Resize trash icon in group admin screen
    style: Fix multiple instances of icons too large
    feat: Port error message parameter to ResponsiveLoginController
    feat: Rebase login route controller and factor out services
    refactor: Provide full functionality of login.inc in the login.html.php template
    Add download service route to routes.php
    Enhance DownloadController with PHPDoc comments
    Implement DownloadController for handling downloads

--- lib/Application.php
+++ lib/Application.php
@@ -39,7 +39,7 @@ if (!class_exists('Horde_Application')) {
     {
         /**
          */
-        public $version = '6.0.0-beta18';
+        public $version = '6.0.0-RC1';
 
         /**
          */
