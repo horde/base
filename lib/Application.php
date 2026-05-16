3907825f10e88520e9b8d8f9e0b205a792c2cb83
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-16 07:40:53 +0200

    chore(release): bump version to 6.0.0-RC2
    Release version 6.0.0-RC2
    
    fix: Require only horde.local.php if exists, not the core.php file
    refactor: Explicitly declare the default stack as a prestep to removing fallback logic
    docs: Mark twitter v1 endpoint deprecated, see horde/base#86
    style: Resize CSS icons in permission admin dialog

--- lib/Application.php
+++ lib/Application.php
@@ -39,7 +39,7 @@ if (!class_exists('Horde_Application')) {
     {
         /**
          */
-        public $version = '6.0.0-RC1';
+        public $version = '6.0.0-RC2';
 
         /**
          */
