fbd52d8144f99ee597827b4c5694e98e8b4d9058
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-25 10:12:42 +0200

    chore(release): bump version to 6.0.0-RC4
    Release version 6.0.0-RC4
    
    Remove symlink from codebase
    fix(auth): resolve login controller DI and compatibility issues
    refactor(responsive): merge responsive rules into screen.css
    refactor(responsive): collapse responsive.css into screen.css
    refactor(routes): replace Mapper with RoutesProvider in UrlGenerator
    refactor(routes): rely on RuntimeRoutesProvider for URL generation
    Merge pull request #94 from horde/feat/adopt-nls-psr4
    refactor: Use PSR-4 version of horde/nls
    Check for existence before echoing version details
    fix: resize icons, closes horde/imp#47
    chore: Update workflow dependency

--- lib/Application.php
+++ lib/Application.php
@@ -39,7 +39,7 @@ if (!class_exists('Horde_Application')) {
     {
         /**
          */
-        public $version = '6.0.0-RC3';
+        public $version = '6.0.0-RC4';
 
         /**
          */
